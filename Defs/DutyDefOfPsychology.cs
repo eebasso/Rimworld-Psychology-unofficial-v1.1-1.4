cfd6e6ddd5563e9d9c00830281fa76587787fe39
eebasso <42080011+eebasso@users.noreply.github.com>
2022-06-18 14:13:13 -0500

    Add files via upload

---
+++ Defs/DutyDefOfPsychology.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse.AI;
+
+namespace Psychology
+{
+    [DefOf]
+    public static class DutyDefOfPsychology
+    {
+        public static DutyDef Vote;
+        public static DutyDef Meeting;
+        public static DutyDef HangOut;
+        public static DutyDef Funeral;
+        //public static DutyDef FollowWarden;
+    }
+}
---
+++ Defs/HediffDefOfPsychology.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Psychology
+{
+    [DefOf]
+    public static class HediffDefOfPsychology
+    {
+        /* Medical conditions */
+        public static HediffDef Anxiety;
+        public static HediffDef MethadoneHigh;
+
+        /* Trait treatment */
+        public static HediffDef RecoveringPyromaniac;
+        public static HediffDef DrugFree;
+        public static HediffDef Antidepressants;
+        public static HediffDef SleepingPills;
+
+        /* Miscellaneous non-health */
+        public static HediffDef HoldingConversation;
+        public static HediffDef Mayor;
+        public static HediffDef PlannedDate;
+        public static HediffDef PlannedFuneral;
+    }
+}
