e95a4bdf46c27d89a770aa5de2018982fcc23517
eebasso <42080011+eebasso@users.noreply.github.com>
2022-06-18 14:33:27 -0500

    Add files via upload

---
+++ Source/1.3/Psychology/Defs/PersonalityNodeDefOf.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+
+namespace Psychology
+{
+    [DefOf]
+    public static class PersonalityNodeDefOf
+    {
+        public static PersonalityNodeDef Aggressive;
+        public static PersonalityNodeDef Ambitious;
+        public static PersonalityNodeDef Compassionate;
+        public static PersonalityNodeDef Competitive;
+        public static PersonalityNodeDef Cool;
+        public static PersonalityNodeDef Empathetic;
+        public static PersonalityNodeDef Experimental;
+        public static PersonalityNodeDef Extroverted;
+        public static PersonalityNodeDef Friendly;
+        public static PersonalityNodeDef Independent;
+        public static PersonalityNodeDef Judgmental;
+        public static PersonalityNodeDef LaidBack;
+        public static PersonalityNodeDef Nostalgic;
+        public static PersonalityNodeDef Outspoken;
+        public static PersonalityNodeDef Organized;
+        public static PersonalityNodeDef Passionate;
+        public static PersonalityNodeDef Polite;
+        public static PersonalityNodeDef Pure;
+        public static PersonalityNodeDef Romantic;
+        public static PersonalityNodeDef Spontaneous;
+        public static PersonalityNodeDef Trusting;
+    }
+}
---
+++ Source/1.3/Psychology/Defs/TaleDefOfPsychology.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace Psychology
+{
+    [DefOf]
+    public static class TaleDefOfPsychology
+    {
+        public static TaleDef TreatedPyromania;
+        public static TaleDef TreatedDrugDesire;
+        public static TaleDef TreatedDepression;
+        public static TaleDef TreatedInsomnia;
+        public static TaleDef CuredAnxiety;
+    }
+}
