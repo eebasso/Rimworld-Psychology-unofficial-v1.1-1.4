526710399bde7ec21108afc2b7c4ca96f621a8e0
eebasso <42080011+eebasso@users.noreply.github.com>
2022-07-13 19:28:01 -0500

    July 13

---
+++ Source/1.3/Psychology/Harmony/EdBPrepareCarefully_PanelBackstory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Verse;
+using HarmonyLib;
+
+namespace Psychology.Harmony
+{
+    [HarmonyPatch(typeof(EdB.PrepareCarefully.PanelBackstory), nameof(EdB.PrepareCarefully.PanelBackstory.DrawRandomizeButton))]
+    public static class EdBPrepareCarefully_PanelTraits_PsychologyPatch
+    {
+        [HarmonyPrefix]
+        public static void DrawRandomizeButton(float y, float width)
+        {
+            Log.Error("DID THIS WORK?");
+            Rect psycheRect = new Rect(width - 102f, y + 9f, 30f, 30f);
+            GUI.DrawTexture(psycheRect, ContentFinder<Texture2D>.Get("Buttons/ButtonPsyche", true));
+            
+        }
+    }
+}
